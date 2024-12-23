871fafb8be0da3902b8c2f9e5c77c08565cb2d90
Vladislav Guzov <sillmaril2@gmail.com>
2024-12-23 11:24:00 +0300

    postgres version

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,14 +62,19 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': os.getenv('DB_NAME'),
         # 'USER': os.getenv('POSTGRES_USER'),
         # 'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         # 'HOST': os.getenv('DB_HOST'),
-        # 'PORT': os.getenv('DB_PORT')
+        # 'PORT': os.getenv('DB_PORT'),
+        'NAME': 'kittygram',
+        'USER': 'kittygram_user',
+        'PASSWORD': 'kittygram_password',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
