e12f66e402abfb3341a6173ff7f6daab9947d517
Vladislav Guzov <sillmaril2@gmail.com>
2024-12-23 11:52:16 +0300

    postgres with env comments delete

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,25 +62,15 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
-        # 'NAME': 'kittygram',
-        # 'USER': 'kittygram_user',
-        # 'PASSWORD': 'kittygram_password',
-        # 'HOST': 'postgres',
-        # 'PORT': '5432',
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
